cb6cd992ab308100fbd30c85b34c4f7538526400
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-14 21:56:41 -0300

    adiciona componente de filtros da Home

---
+++ src/components/Home/FilterBar.tsx
@@ -0,0 +1,12 @@
+function FilterBar() {
+    return (
+        <>
+            <input type="text" />
+            <button>Todas</button>
+            <button>Pendentes</button>
+            <button>Concluídas</button>
+        </>
+    )
+}
+
+export default FilterBar;
