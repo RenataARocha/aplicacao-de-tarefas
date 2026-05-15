0996623c8b3178deb03a85c42f9c93ca2130da03
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-15 00:07:07 -0300

    feat: adiciona lógica dinâmica aos cards de resumo

--- src/components/Home/SummaryCards/SummaryCard.tsx
+++ src/components/Home/SummaryCards/SummaryCard.tsx
@@ -1,20 +1,26 @@
-function SummaryCards() {
+import type { Task } from "../../../types/task"
+
+type SummaryCardsProps = {
+    tasks: Task[];
+}
+
+function SummaryCards({ tasks }: SummaryCardsProps) {
     return (
         <>
             <section>
                 <div>
                     <p>Total</p>
-                    <h2>1</h2>
+                    <h2>{tasks.length}</h2>
                 </div>
 
                 <div>
                     <p>Pendentes</p>
-                    <h2>3</h2>
+                    <h2>{tasks.filter(task => task.concluida === false).length}</h2>
                 </div>
 
                 <div>
                     <p>Concluídas</p>
-                    <h2>2</h2>
+                    <h2>{tasks.filter(task => task.concluida === true).length}</h2>
                 </div>
             </section>
         </>
