de6c6c8ef791a63026620e89da58c4cf90736c5d
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-14 23:20:43 -0300

    configura rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import Home from "./pages/Home"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NewTask from './pages/NewTask'
 
 
@@ -8,8 +8,12 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Home />
-      <NewTask />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/nova-tarefa" element={<NewTask />}></Route>
+      </Routes>
+
+
     </BrowserRouter>
   )
 }
