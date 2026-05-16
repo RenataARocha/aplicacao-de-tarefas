eee380eb163e7e8bc4e7a44f563804afe8b30936
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-15 21:58:53 -0300

    feat: adiciona persistência de tarefas com localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,36 @@ import Home from "./pages/Home"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NewTask from './pages/NewTask'
 
+import { useEffect, useState } from "react"
+import type { Task } from "./types/task"
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>(() => {
+
+    const tarefasSalvas = localStorage.getItem("tasks")
+
+    return tarefasSalvas ? JSON.parse(tarefasSalvas) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
+  function handleAdicionarTask(newTask: Task) {
+    setTasks([...tasks, newTask])
+
+    console.log(newTask)
+  }
 
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/nova-tarefa" element={<NewTask />}></Route>
+
+        <Route path="/" element={<Home tasks={tasks} />}></Route>
+        <Route path="/nova-tarefa" element={<NewTask onAddTask={handleAdicionarTask} />}></Route>
       </Routes>
+
 
 
     </BrowserRouter>
