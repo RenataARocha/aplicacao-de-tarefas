f7980ed365d48b3d0925f51f3eb71a2f4921bd76
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-15 21:59:01 -0300

    feat: implementa formulário funcional para criação de tarefas

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,4 +1,43 @@
-function NewTask() {
+import { useState } from "react";
+import type { Task } from "../types/task";
+import { useNavigate } from "react-router-dom";
+
+type NewTaskProps = {
+    onAddTask: (newTask: Task) => void;
+}
+
+function NewTask({ onAddTask }: NewTaskProps) {
+
+    const [titulo, setTitulo] = useState("")
+    const [descricao, setDescricao] = useState("")
+    const [prioridade, setPrioridade] = useState("")
+    const [data, setData] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSalvarTarefa(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const newTask = {
+            id: Date.now(),
+            titulo,
+            descricao,
+            prioridade,
+            concluida: false,
+            data
+        }
+
+        onAddTask(newTask)
+        navigate("/")
+
+        setTitulo("")
+        setDescricao("")
+        setPrioridade("")
+        setData("")
+    }
+
+
+
     return (
         <>
             <main>
@@ -7,23 +46,23 @@ function NewTask() {
                     <h1>Nova tarefa</h1>
                     <p>Organize uma nova ideia em segundos.</p>
 
-                    <form >
+                    <form onSubmit={handleSalvarTarefa}>
                         <label htmlFor="input-tarefa">TÍTULO</label>
-                        <input type="text" id="input-tarefa" placeholder="Ex.: Finalizar protólipo" />
+                        <input type="text" id="input-tarefa" value={titulo} onChange={event => setTitulo(event.target.value)} placeholder="Ex.: Finalizar protólipo" />
 
                         <label htmlFor="descricao-tarefa">DESCRIÇÃO</label>
-                        <textarea name="" id="descricao-tarefa" placeholder="Detalhes opcionais sobre a tarefa..."></textarea>
+                        <textarea name="" id="descricao-tarefa" value={descricao} onChange={event => setDescricao(event.target.value)} placeholder="Detalhes opcionais sobre a tarefa..."></textarea>
 
                         <div>
                             <h2>PRIORIDADE</h2>
-                            <button type="button">Baixa</button>
-                            <button type="button">Média</button>
-                            <button type="button">Alta</button>
+                            <button type="button" onClick={() => setPrioridade("Baixa")}>Baixa</button>
+                            <button type="button" onClick={() => setPrioridade("Média")}>Média</button>
+                            <button type="button" onClick={() => setPrioridade("Alta")}>Alta</button>
                         </div>
 
                         <div>
                             <h2>DATA</h2>
-                            <input type="date" />
+                            <input type="date" value={data} onChange={event => setData(event.target.value)} />
                         </div>
 
                         <div>
