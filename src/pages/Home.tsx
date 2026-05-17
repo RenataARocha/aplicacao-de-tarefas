55618611a60462242f6bc95ff8b0b8d5e944f7ff
RenataARocha <programadorawebrenatarocha@gmail.com>
2026-05-16 23:36:39 -0300

    style(home): melhorar layout, botão adicionar e responsividade

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import TaskList from "../components/Home/TaskList/TaskList"
 import { useContext } from "react"
 import { TaskContext } from "../context/TaskContext"
 import { Link } from "react-router-dom"
+import { Sparkles } from "lucide-react";
+import "./Home.css"
 
 function Home() {
 
@@ -14,11 +16,16 @@ function Home() {
         <>
             <main>
                 <section>
-                    <p>Seu fluxo, no seu ritmo</p>
-                    <h1>Suas tarefas</h1>
-                    <p>Você tem {tasks.filter(task => !task.concluida).length} tarefas pendentes.</p>
+                    <div className="conteinerFluxo">
+                        <p className="subFluxo">
+                            <Sparkles size={16} />
+                            Seu fluxo, no seu ritmo
+                        </p>
+                        <h1>Suas <span>tarefas</span></h1>
+                        <p className="tarefas-pendentes">Você tem {tasks.filter(task => !task.concluida).length} tarefas pendentes.</p>
+                    </div>
 
-                    <Link to="/nova-tarefa">+ Adicionar Tarefa</Link>
+                    <Link to="/nova-tarefa" className="btnAdicionar">+ Adicionar Tarefa</Link>
                 </section>
             </main>
             <SummaryCard />
